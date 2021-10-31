44c14dc23f5c9b5e1d639f83c5f1edece1fa262f
Bandi Jai Krishna <49020643+TextZip@users.noreply.github.com>
2021-10-31 12:23:49 +0530

    fixed issues in potential node list

--- main.py
+++ main.py
@@ -17,29 +17,16 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 from numpy.core.fromnumeric import repeat
 
-grid = np.array([[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],  # Row 0
-                 [0, 1, 1, 0, 0, 0, 0, 1, 0, 0],  # Row 1
-                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # Row 2
-                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # Row 3
-                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # Row 4
-                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # Row 5
-                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # Row 6
-                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # Row 7
-                 [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # Row 8
-                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) # Row 9
-        # Columns 0  1  2  3  4  5  6  7  8  9
-
-
 class GraphPlanner:
-    def __init__(self, start, goal, method, connectivity, debug):
+    def __init__(self, start, goal, method, connectivity, nrows = 10, ncolumns = 10, debug=True, obstacles = []):
         self.start = start
         self.position = start
         # print(self.position[0]+1)
         self.goal = goal
         self.goal_str = str(self.goal)
         self.position_str = str(self.position)
-        self.nrows = 10
-        self.ncolumns = 10
+        self.nrows = nrows
+        self.ncolumns = ncolumns
         self.game_over = False
         self.potential_list = []
 
@@ -53,12 +40,12 @@ class GraphPlanner:
         self.heuristic = 0
         self.explored = []
         self.explored.append(self.position)
-        self.obstacles = [[1,1],[2,1],[3,1]]
+        self.obstacles = obstacles
 
     def get_potential_moves(self):
         # list potential moves based on connectivity
         if self.connectivity == 8:
-            self.potential_list = [[self.position[0] - 1, self.position[1]-1],
+            self.pre_potential_list = [[self.position[0] - 1, self.position[1]-1],
                                    [self.position[0]+1, self.position[1]+1],
                                    [self.position[0], self.position[1]-1],
                                    [self.position[0]-1, self.position[1]],
@@ -67,27 +54,26 @@ class GraphPlanner:
                                    [self.position[0]-1, self.position[1]+1],
                                    [self.position[0]+1, self.position[1]-1]]
         if self.connectivity == 4:
-            self.potential_list = [[self.position[0], self.position[1]-1],
+            self.pre_potential_list = [[self.position[0], self.position[1]-1],
                                    [self.position[0]-1, self.position[1]],
                                    [self.position[0]+1, self.position[1]],
                                    [self.position[0], self.position[1]+1]]
         # check borders
-        for item in self.potential_list:
-            if item[0] <= 0 or item[0] >= self.nrows:
-                self.potential_list.remove(item)
-        for item in self.potential_list:
-            if item[1] <= 0 or item[1] >= self.ncolumns:
-                self.potential_list.remove(item)
+        for item in self.pre_potential_list:
+            if (item[0] >= 0 and item[0] <= self.nrows-1) and (item[1] >= 0 and item[1] <= self.ncolumns-1):
+                self.potential_list.append(item)
         # filter out potential moves based on obstacles/wall
         for item in self.obstacles:
             if item in self.potential_list:
                 self.potential_list.remove(item)
             else:
                 pass
+        # print(self.potential_list)
         # filter out potential movies based on explored tiles
         for item in self.explored:
             if item in self.potential_list:
                 self.potential_list.remove(item)
+        # print(self.potential_list)
         # check if goal is in potential moves ?
         if self.goal in self.potential_list:
             print("game over")
@@ -118,6 +104,7 @@ class GraphPlanner:
         # take top most element from the sorted_list and push as the current position and pop the element as well
         # print(f"sorted list: {sorted_list}")
         # change current position to the new node
+        # print(sorted_list[0])
         self.position = self.string_to_array(sorted_list[0])
         self.position_str = sorted_list[0]
         self.explored.append(self.string_to_array(
@@ -132,15 +119,15 @@ class GraphPlanner:
 
 
 if __name__ == "__main__":
-    planner = GraphPlanner(start=[0,0],goal=[5, 9], method='A_star',
-                           connectivity=8, debug=True)
+    planner = GraphPlanner(start=[0,1],goal=[5, 9], method='A_star',
+                           connectivity=8, obstacles=[[1,1],[1,2],[1,3],[3,1],[2,1],[4,1]])
     def animate(i):
         #data = np.random.rand(10, 10) * 20 #create a zero matrix
-        data = np.zeros([planner.nrows,planner.ncolumns],dtype=int)
+        data = np.zeros((planner.nrows,planner.ncolumns),dtype=int)
         if planner.game_over == False:
             planner.get_potential_moves()
             planner.make_move()
-            print(planner.path)
+            # print(planner.path)
         else:
             pass
             # global data
@@ -148,8 +135,6 @@ if __name__ == "__main__":
         for item in planner.frontier:
             int_item = planner.string_to_array(item)
             data[int_item[0],int_item[1]] = 22
-        # format for present
-        data[planner.position[0],planner.position[1]] = 27
         # format for explored
         for item in planner.explored:
             data[item[0],item[1]] = 32
@@ -158,11 +143,13 @@ if __name__ == "__main__":
         # format for obstacles
         for item in planner.obstacles:
             data[item[0],item[1]] = 37 
+        # format for present
+        # data[planner.position[0],planner.position[1]] = 27
         # format for start location
         data[planner.start[0],planner.start[1]] = 12
         # format for goal location
         data[planner.goal[0],planner.goal[1]] = 17
-        cmap = colors.ListedColormap(['white','red','green','yellow','blue','gray','black'])
+        cmap = colors.ListedColormap(['white','red','green','yellow','blue','gray','brown'])
         bounds = [0,10,15,20,25,30,35,40]
         # start -  red
         # goal - green
@@ -179,8 +166,8 @@ if __name__ == "__main__":
 
             # draw gridlines
         ax.grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
-        ax.set_xticks(np.arange(-.5, 10.5, 1))
-        ax.set_yticks(np.arange(-.5, 10.5, 1))
+        ax.set_xticks(np.arange(-.5, planner.ncolumns + 0.5, 1))
+        ax.set_yticks(np.arange(-.5, planner.nrows + 0.5, 1))
         ax.axes.xaxis.set_ticklabels([])
         ax.axes.yaxis.set_ticklabels([])
 
@@ -189,6 +176,6 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots()
     fig.canvas.mpl_connect('button_press_event', onClick)
-    anim = FuncAnimation(fig, animate)
+    anim = FuncAnimation(fig, animate, interval=20)
     plt.show()
 
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-potential_list = [[1,1],[1,0],[-1,0],[0,-1]]
+potential_list = [[1,1],[1,0],[-1,0],[0,-1],[-3,1]]
 print(potential_list)
 
 for item1,item2 in potential_list:
-    if item2 < 0:
+    if item2 <= 0:
         potential_list.remove([item1,item2])
 
 for item1,item2 in potential_list:
-    if item1 < 0:
+    if item1 <= 0:
         potential_list.remove([item1,item2])
 
 print(potential_list)
